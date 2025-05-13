a1e5f2b846d34c0eb8185b7821b110af89da6f9b
AirinNila <airinakhternila>
2025-05-14 00:40:48 +0600

    fixed nav

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -3,10 +3,12 @@ import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div className='flex justify-between items-center border-b-2 p-2'>
+        <header className='container mx-auto'>
+        <div className='flex justify-between items-center border-b-2 p-2 '>
               <h1 className=' text-3xl font-semibold'>Knowladge cafe</h1>
               <img src={profile} alt="" />
         </div>
+        </header>
     );
 };
 
